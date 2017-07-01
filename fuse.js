5a5539a9cdadd0702bfd5091a9524707745b835b
Christian Todd <christiantodd27@icloud.com>
2017-07-01 14:55:19 -0400

    inital commit.

---
+++ fuse.js
@@ -0,0 +1,53 @@
+const {
+  FuseBox,
+  CSSPlugin,
+  CSSModules,
+  BabelPlugin,
+  QuantumPlugin,
+  WebIndexPlugin,
+  UglifyJSPlugin,
+  Sparky
+} = require('fuse-box');
+
+let fuse,
+  app,
+  vendor,
+  isProduction = false;
+
+Sparky.task('config', () => {
+  fuse = new FuseBox({
+    homeDir: 'src/',
+    output: 'dist/$name.js',
+    hash: isProduction,
+    sourceMaps: !isProduction,
+    plugins: [
+      [CSSModules(), CSSPlugin()],
+      BabelPlugin(),
+      WebIndexPlugin({ template: 'src/index.html' })
+    ]
+  });
+  // VENDOR BUNDLE (FOR REACT)
+  vendor = fuse.bundle('vendor').instructions('~ index.js');
+
+  // MAIN BUNDLE
+  app = fuse.bundle('app').instructions('> [index.js]');
+});
+
+// RUN WITH YARN START
+Sparky.task('default', ['clean', 'config'], () => {
+  fuse.dev();
+  // add dev instructions
+  app.watch().hmr();
+  return fuse.run();
+});
+
+// RUN WITH YARN DIST
+Sparky.task('dist', ['prod-env', 'config'], () => {
+  return fuse.run();
+});
+
+// SPARKY TASKS
+Sparky.task('clean', () => Sparky.src('dist/').clean('dist/'));
+Sparky.task('prod-env', ['clean'], () => {
+  isProduction = true;
+});
---
+++ src/components/app.jsx
@@ -0,0 +1,12 @@
+import { h, Component } from 'preact';
+
+import './app.css';
+export default class App extends Component {
+  render() {
+    return (
+      <div styleName="card">
+        <h1>Hello, World</h1>
+      </div>
+    );
+  }
+}
