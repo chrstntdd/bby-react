948fae67a17ce8fadd4e7f6f8ced45361f717495
Christian Todd <christiantodd27@icloud.com>
2017-07-04 13:07:04 -0400

    update server entry point with constants and server/db  helper functions.

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
+require('dotenv').config();
+
 const express = require('express'),
   app = express(),
+  bodyParser = require('body-parser'),
+  mongoose = require('mongoose'),
   logger = require('morgan'),
-  config = require('./config/main');
+  router = require('./router');
 
-// START THE SERVER
-const server = app.listen(config.port);
-console.log(`Your server is up and running on port ${config.port}`);
+const DATABASE_URL = process.env.DATABASE_URL;
+const PORT = process.env.PORT || 27017;
+const JWT_SECRET = process.env.JWT_SECRET;
 
 // MIDDLEWARE STACK
 app.use((req, res, next) => {
@@ -18,3 +22,56 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Credentials', 'true');
   next();
 });
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+mongoose.Promise = global.Promise;
+
+// closeServer needs access to a server object, but that only
+// gets created when `runServer` runs, so we declare `server` here
+// and then assign a value to it on run
+let server;
+
+// TAKES A DATABASE URL AS AN ARGUMENT. NEEDED FOR INTEGRATION TESTS. DEFAULTS TO THE MAIN URL.
+const runServer = (databaseUrl = DATABASE_URL, port = PORT) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(databaseUrl, { useMongoClient: true }, err => {
+      if (err) {
+        return reject(err);
+      }
+      server = app
+        .listen(port, () => {
+          console.info(`Your app is listening on port ${port}. 🤔`);
+          resolve();
+        })
+        .on('error', err => {
+          mongoose.disconnect();
+          reject(err);
+        });
+    });
+  });
+
+const closeServer = () =>
+  mongoose.disconnect().then(() => {
+    return new Promise((resolve, reject) => {
+      console.info('Closing server. Goodbye old friend.');
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        return resolve();
+      });
+    });
+  });
+
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+}
+
+module.exports = {
+  runServer,
+  closeServer,
+  app
+};
+
+router(app);
