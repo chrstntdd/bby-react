a4d4467f3b3c06a68e9c5b884a8d0feb00da8f2a
Christian Todd <christiantodd27@icloud.com>
2017-07-05 00:22:46 -0400

    add redux dependencies and connect store to client entry point.

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { Router, browserHistory } from 'react-router';
+import reduxThunk from 'redux-thunk';
+import routes from './routes';
+import reducers from './reducers/index';
+import { AUTH_USER } from './actions/types';
+
+const createStoreWithMiddleware = applyMiddleware(reduxThunk)(createStore);
+const store = createStoreWithMiddleware(reducers);
 
 import './index.css';
 
 import App from './components/app';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={browserHistory} routes={routes} />
+  </Provider>,
+  document.getElementById('root')
+);
