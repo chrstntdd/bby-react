b9fd5808a0e012cceba6503eacb41835c222bc11
chrstntdd <christiantodd27@icloud.com>
2017-07-17 00:25:13 -0400

    clear upc input on successful post actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,11 +1,24 @@
 import { combineReducers } from 'redux';
-import { reducer as formReducer } from 'redux-form';
+import { POST_UPC, INCREMENT_PRODUCT_QUANTITY } from '../actions/types';
+import { reducer as formReducer, change as CHANGE } from 'redux-form';
 import authReducer from './auth_reducer';
 import bbyAPIReducer from './bby_reducer';
 
 const rootReducer = combineReducers({
   auth: authReducer,
-  form: formReducer,
+  form: formReducer.plugin({
+    // CLEAR INPUT AFTER FORM SUBMIT SUCCEEDS
+    postUPC: (state, action) => {
+      switch (action.type) {
+        case POST_UPC:
+          return undefined;
+        case INCREMENT_PRODUCT_QUANTITY:
+          return undefined;
+        default:
+          return state;
+      }
+    }
+  }),
   table: bbyAPIReducer
 });
 
