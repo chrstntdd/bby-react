d851defb42df6e67ae2bb37172705dedaa1145b4
chrstntdd <christiantodd27@icloud.com>
2017-07-17 21:43:41 -0400

    implement auto-submit form and minor validation. BUG: form doesnt reset on INCREMENT_PRODUCT_QUANTITY.

--- client/src/components/search-bar.jsx
+++ client/src/components/search-bar.jsx
@@ -4,20 +4,40 @@ import { Field, reduxForm } from 'redux-form';
 import { withRouter } from 'react-router-dom';
 import { getProductDetails, formatTable } from '../actions';
 
+import './search-bar.scss';
+
 const form = reduxForm({
   form: 'postUPC'
 });
 
 export class SearchBar extends React.Component {
-  handleFormSubmit(formProps) {
-    this.props.getProductDetails(formProps);
+  handleInputChange(formProps) {
+    const inputValue = formProps.currentTarget.value;
+
+    if (!isNaN(Number(inputValue)) && inputValue.length == 12) {
+      // IF VALID UPC
+      this.handleFormSubmit(inputValue);
+    } else if (isNaN(Number(inputValue))) {
+      // IF ANY OF THE VALUES ARE NOT NUMBERS
+      alert(`Looks like you didn't quite scan the UPC. Try again please.`);
+    } else if (inputValue.length < 12) {
+      // IF THE INPUT LENGTH IS SHORTER THAN 12
+      console.log(`We need a valid UPC my guy.`);
+    }
   }
+
+  handleFormSubmit(inputValue) {
+    this.props.getProductDetails({ upc: inputValue.toString() });
+  }
+
   handleTableFormat() {
     this.props.formatTable();
   }
+
   handlePrintTable() {
     window.print();
   }
+
   renderAlert() {
     if (this.props.errorMessage) {
       return (
@@ -31,21 +51,23 @@ export class SearchBar extends React.Component {
     }
   }
   render() {
-    const { handleSubmit } = this.props;
+    const { onChange } = this.props;
     return (
       <section>
-        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+        <form>
           {this.renderAlert()}
           <div className="input-group">
             <Field
+              id="upcInput"
+              onChange={this.handleInputChange.bind(this)}
+              autoComplete="false"
               name="upc"
               component="input"
-              type="number"
-              placeholder="Enter UPC"
+              placeholder="UPC"
             />
-            <label htmlFor="upc">UPC</label>
           </div>
         </form>
+
         <button onClick={() => this.handleTableFormat()}> FORMAT TABLE</button>
         <button onClick={() => this.handlePrintTable()}> PRINT TABLE</button>
       </section>
@@ -58,5 +80,8 @@ const mapStateToProps = state => ({
 });
 
 export default withRouter(
-  connect(mapStateToProps, { getProductDetails, formatTable })(form(SearchBar))
+  connect(mapStateToProps, {
+    getProductDetails,
+    formatTable
+  })(form(SearchBar))
 );
