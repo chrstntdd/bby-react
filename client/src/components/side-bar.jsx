381432ba1c66c11f2cc78d02bdbd49d49c047c9f
chrstntdd <christiantodd27@icloud.com>
2017-07-20 23:14:12 -0400

    complete sidebar component.

---
+++ client/src/components/side-bar.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { formatTable, printTable } from '../actions';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
+
+import printIcon from '../static/noun_772280_cc.svg';
+import formatIcon from '../static/noun_796398_cc.svg';
+import undoButton from '../static/noun_1031806_cc.svg';
+import redoButton from '../static/noun_1031802_cc.svg';
+import clearIcon from '../static/noun_926276_cc.svg';
+
+import './side-bar.scss';
+
+export class SideBar extends React.Component {
+  handleTableFormat() {
+    this.props.formatTable();
+  }
+
+  handlePrintTable() {
+    window.print();
+  }
+  render() {
+    return (
+      <aside id="side-bar">
+        <div className="btn-container">
+          <button>
+            <img src={undoButton} alt="" />
+            <p>Undo</p>
+          </button>
+        </div>
+        <div className="btn-container">
+          <button>
+            <img src={redoButton} alt="" />
+            <p>Redo</p>
+          </button>
+        </div>
+        <div className="btn-container">
+          {' '}<button onClick={() => this.handleTableFormat()}>
+            <img src={formatIcon} alt="" />
+            <p>Format</p>
+          </button>
+        </div>
+        <div className="btn-container">
+          <button onClick={() => this.handlePrintTable()}>
+            <img src={printIcon} alt="" />
+            <p>Print</p>
+          </button>
+        </div>
+        <div className="btn-container">
+          {' '}<button>
+            <img id="clear-icon" src={clearIcon} alt="" />
+            <p>Clear</p>
+          </button>
+        </div>
+      </aside>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  products: state.table.products
+});
+
+export default withRouter(connect(mapStateToProps, { formatTable })(SideBar));
