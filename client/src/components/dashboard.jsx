453459538fd81199f3a87519f90e7334df26882e
chrstntdd <christiantodd27@icloud.com>
2017-07-21 00:28:58 -0400

    adjust layout.

--- client/src/components/dashboard.jsx
+++ client/src/components/dashboard.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { withRouter, Link } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 
 // CHILD COMPONENTS
 import SearchBar from './search-bar';
 import ProductTable from './product-table';
+import SideBar from './side-bar';
+import DashboardHeader from './dashboard-header';
+
+import './dashboard.scss';
 
 export class Dashboard extends React.Component {
   render() {
     return (
-      <div>
-        <h1>DASHBOARD</h1>
-        <Link to="/logout">
-          <button>Logout</button>
-        </Link>
-        <SearchBar />
-        <ProductTable />
+      <div id="dashboard-body">
+        <DashboardHeader />
+        <div id="main-content-area">
+          <SideBar />
+          <section id="main-table-area">
+            <SearchBar />
+            <ProductTable />
+          </section>
+        </div>
       </div>
     );
   }
