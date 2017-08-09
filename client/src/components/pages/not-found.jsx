5dab9abbf904482d32f69b25eaeb29fe4a8c4ec4
chrstntdd <christiantodd27@icloud.com>
2017-08-08 23:22:09 -0400

    update 404 page.

--- client/src/components/pages/not-found.jsx
+++ client/src/components/pages/not-found.jsx
@@ -1,11 +1,30 @@
 import React from 'react';
+import './not-found.scss';
+import { Link } from 'react-router-dom';
 
 export default class NotFoundPage extends React.Component {
+  handleBackToSafety() {
+    this.props.history.push('/dashboard');
+  }
   render() {
     return (
       <div>
-        <h1>404 - Page Not Found</h1>
-        <p>I'm sorry, the page you were looking for cannot be found!</p>
+        <svg viewBox="0 0 600 300">
+          <symbol id="s-text">
+            <text textAnchor="middle" x="50%" y="50%" dy=".35em">
+              404 ERROR
+            </text>
+          </symbol>
+          <use className="text" xlinkHref="#s-text" />
+          <use className="text" xlinkHref="#s-text" />
+          <use className="text" xlinkHref="#s-text" />
+          <use className="text" xlinkHref="#s-text" />
+          <use className="text" xlinkHref="#s-text" />
+        </svg>
+
+        <button id="safety" onClick={() => this.handleBackToSafety()}>
+          Back to Safety
+        </button>
       </div>
     );
   }
