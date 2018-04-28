ba827d613e27bf35858d8a8764452922b0e6b234
Christian Todd <christiantodd27@icloud.com>
2018-04-28 18:19:28 -0400

    fix path resolution error.

--- src/ui/pages/Register/index.tsx
+++ src/ui/pages/Register/index.tsx
@@ -3,7 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
 import LoadingIndicator from '@/ui/components/Loading';
-import RegisterForm from '@/ui/pages/Register/RegisterForm/Index';
+import RegisterForm from '@/ui/pages/Register/RegisterForm';
 
 import './register.scss';
 
